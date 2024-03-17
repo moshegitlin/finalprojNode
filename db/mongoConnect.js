b6a769d9162d77820f7985b90a33d8f88efedacb
Moshe Gitlin <jmoshe052@gmail.com>
2024-03-17 08:38:37 +0200

    a

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -1,10 +1,11 @@
 // getting-started.js
 const mongoose = require('mongoose');
+const { config } = require('../middlewares/secret');
 
 main().catch(err => console.log(err));
 
 async function main() {
     mongoose.set('strictQuery', false);
-    await mongoose.connect('mongodb://127.0.0.1:27017/finalProject');
+    await mongoose.connect(config.db_url);
     console.log('mongo connected');
 }
--- middlewares/secret.js
+++ middlewares/secret.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 exports.config = {
-    tokenSecret:process.env.TOKEN_SECRET,
-    db_pass:"",
-    db_user:"",
-    db_url:""
+    tokenSecret: process.env.TOKEN_SECRET,
+    db_pass: "",
+    db_user: "",
+    db_url: process.env.DB_URL
 }
